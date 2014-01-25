7991013e55b741363c515385f7375968ff8edaa3
Wiktor Mociun <voter101@gmail.com>
2014-01-25 18:07:39 +0100

    Static styles serving

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.template
 import tornado.options
+import os
 
 from List import *
 
@@ -17,11 +18,13 @@ class NextSuckerHandler(tornado.web.RequestHandler):
         result = nextSucker(duty, dutyMap)
         self.write(json.dumps(getSucker(duty,dutyMap)))
 
-
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+}
 application = tornado.web.Application([
     (r"/", MainHandler),    
     (r"/nextSucker/([a-z]+)", NextSuckerHandler),
-])
+], **settings)
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
