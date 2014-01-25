3316dc5ebddcf5af1267e90bda50cf8aad1d8147
Wiktor Mociun <voter101@gmail.com>
2014-01-25 19:27:28 +0100

    Server redesign

--- server.py
+++ server.py
@@ -2,9 +2,12 @@ import tornado.ioloop
 import tornado.web
 import tornado.template
 import tornado.options
+import tornado.httpserver
 import os
 
 from List import *
+from tornado.options import define
+define("port", default=8888, help="Run on given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -18,15 +21,22 @@ class NextSuckerHandler(tornado.web.RequestHandler):
         result = nextSucker(duty, dutyMap)
         self.write(json.dumps(getSucker(duty,dutyMap)))
 
-settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
-}
-application = tornado.web.Application([
-    (r"/", MainHandler),    
-    (r"/nextSucker/([a-z]+)", NextSuckerHandler),
-], **settings)
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),    
+            (r"/nextSucker/([a-z]+)", NextSuckerHandler),
+        ]
+        settings = {
+            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(tornado.options.options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+    main()
